807511aa9ea12eaa8bbe94897b42ebf9bee76fe6
bartezic <bartezic@gmail.com>
2013-05-20 18:43:49 +0300

    now popular countries is popular

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,19 +9,12 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery
   before_filter :set_locale
- 
-  # def set_locale
-  #   I18n.locale = params[:locale] || I18n.default_locale
-  # end
 
   def set_locale
     session['locale'] = params[:locale] if params[:locale]
     I18n.locale = (session.has_key? 'locale' || !session['locale'].empty?) ? session['locale'] : I18n.default_locale
   end
 
-  # def set_admin_locale
-  #   I18n.locale = :en
-  # end
   private
   
   def render_404(exception)
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,12 +3,10 @@ class StaticController < ApplicationController
     # @ukraine = Country.where(code: :ua).first
     # @regions = Region.where('country_id != ?', @ukraine.id).order('id DESC').limit(5) 
 
-    res = TourProgram.all.group_by(&:region_ids)
-    res2 = {}
-    res.each{ |k,v| k.each{|i| res2[i] = v.uniq.size}}
-    res2 = res2.sort_by(&:last).map(&:first).uniq.last(5)
-    @regions = Region.find(res2)
-    @countries = Country.limit(4)
+    regions = popular_regions
+
+    @regions = regions.sort_by(&:last).map(&:first).uniq.last(5)
+    @countries = popular_countries(regions).sort_by(&:last).map(&:first).uniq.last(4)
 
     @tours = Tour.search(params).limit(5)
     @hot_tours = Tour.search(params.merge({ tour_type: TourType.where(code: :hot).map(&:id) }))
@@ -37,5 +35,27 @@ class StaticController < ApplicationController
       format.html { render template: 'errors/404', layout: 'layouts/application', status: 404 }
       format.all { render nothing: true, status: 404 }
     end
+  end
+
+  private
+  # returned array of Hash in format { region => tours_count }
+  def popular_regions
+    # res =TourProgram.all.group_by(&:region_ids).inject({}){ |mem, (k,v)| k.each{|i| mem[i] = v.uniq.size; }; mem}
+    # res = TourProgram.all.group_by(&:region_ids).inject({}){ |mem, i| i}
+    # res.each{ |k,v| k.each{|i| res2[i] = v.uniq.size}}
+    res = {}
+    TourProgram.all.group_by(&:region_ids).each{ |k,v| k.each{|i| res[i] = v.uniq.size}}
+    Region.find(res.map(&:first)).inject({}){ |mem, region| 
+      mem[region] = res[region.id] 
+      mem 
+    }
+  end
+
+  # regions it is array of hash of popular regions in format { region => tours_count }
+  def popular_countries(regions)
+    Country.includes(:regions).where('regions.id IN (?)', regions.map(&:first).map(&:id)).inject({}){|hash, country|
+      hash[country] = country.regions.inject(0){ |mem, region| mem += regions[region] }
+      hash
+    }
   end
 end
