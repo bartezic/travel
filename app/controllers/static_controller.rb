0be1918ae5851aa004d598f1c6d163beebb71a4c
bartezic <bartezic@gmail.com>
2013-05-20 19:18:59 +0300

    now popular countries is popular - refactoring

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,11 +3,8 @@ class StaticController < ApplicationController
     # @ukraine = Country.where(code: :ua).first
     # @regions = Region.where('country_id != ?', @ukraine.id).order('id DESC').limit(5) 
 
-    regions = popular_regions
-
-    @regions = regions.sort_by(&:last).map(&:first).uniq.last(5)
-    @countries = popular_countries(regions).sort_by(&:last).map(&:first).uniq.last(4)
-
+    @regions = popular_regions
+    @countries = popular_countries
     @tours = Tour.search(params).limit(5)
     @hot_tours = Tour.search(params.merge({ tour_type: TourType.where(code: :hot).map(&:id) }))
     respond_to do |format|
@@ -40,22 +37,17 @@ class StaticController < ApplicationController
   private
   # returned array of Hash in format { region => tours_count }
   def popular_regions
-    # res =TourProgram.all.group_by(&:region_ids).inject({}){ |mem, (k,v)| k.each{|i| mem[i] = v.uniq.size; }; mem}
-    # res = TourProgram.all.group_by(&:region_ids).inject({}){ |mem, i| i}
-    # res.each{ |k,v| k.each{|i| res2[i] = v.uniq.size}}
-    res = {}
-    TourProgram.all.group_by(&:region_ids).each{ |k,v| k.each{|i| res[i] = v.uniq.size}}
-    Region.find(res.map(&:first)).inject({}){ |mem, region| 
-      mem[region] = res[region.id] 
-      mem 
-    }
+    Region.includes(:tour_programs).inject({}){|mem, region| 
+      mem[region] = region.tour_programs.map(&:tour_id).uniq.size; 
+      mem
+    }.sort_by(&:last).map(&:first).uniq.last(5).reverse
   end
 
   # regions it is array of hash of popular regions in format { region => tours_count }
-  def popular_countries(regions)
-    Country.includes(:regions).where('regions.id IN (?)', regions.map(&:first).map(&:id)).inject({}){|hash, country|
-      hash[country] = country.regions.inject(0){ |mem, region| mem += regions[region] }
-      hash
-    }
+  def popular_countries
+    Country.includes(:regions, :regions => :tour_programs).inject({}){|mem, country|
+      mem[country] = country.regions.map{ |region| region.tour_programs.map(&:tour_id) }.flatten.uniq.size
+      mem
+    }.sort_by(&:last).map(&:first).uniq.last(3).reverse
   end
 end
