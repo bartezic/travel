3cdc89a5d46b2ede341bfba9ce7bbe6635423bd5
Viktor Oleksyn <bartezic@inbox.ru>
2013-06-02 15:04:03 +0300

    try to fix fb_graph

--- app/admin/photos.rb
+++ app/admin/photos.rb
@@ -2,8 +2,8 @@ ActiveAdmin.register Photo do
   menu :priority => 8, :label => proc{ I18n.t('active_admin.menu.photos') }
 
   filter :id
-  filter :meta_galleries, :as => :select, :collection => Gallery.all
-  filter :meta_countries, :as => :select, :collection => Country.all
+  filter :meta_galleries, :as => :select, :collection => proc{ Gallery.all }
+  filter :meta_countries, :as => :select, :collection => proc{ Country.all }
   filter :title
   filter :slug
 
