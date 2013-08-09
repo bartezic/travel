1dfde9cd5544535c391e2ee5042793ddf8caf052
bartezic <bartezic@gmail.com>
2013-08-09 23:55:54 +0300

    few fixes

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,8 +1,8 @@
 class ToursController < InheritedResources::Base
   def show
     @tour = Tour.find(params[:id])
-    regions_ids = @tour.tour_programs.map(&:region_ids).flatten
-    @related = Tour.search(params.merge({ region: regions_ids })).where('tours.id != ?', @tour.id).limit(5)
+    regions_ids = Country.joins(:regions).where('regions.id in (?)', @tour.tour_programs.map(&:region_ids).flatten).map(&:region_ids).flatten
+    @related = Tour.search(params.merge({ region: regions_ids, tour_typ: @tour.tour_types.map(&:id) })).where('tours.id != ?', @tour.id).limit(12)
   end
 
   def index
