626b18d312a9029ad311683234a36ce11a9379d1
bartezic <bartezic@gmail.com>
2013-08-09 22:09:52 +0300

    fixed db cresh

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -1,7 +1,7 @@
 set_default(:postgresql_host, "localhost")
 set_default(:postgresql_user) { application }
 # set_default(:postgresql_password) { Capistrano::CLI.password_prompt "PostgreSQL Password: " }
-set_default(:postgresql_password) { "#{application}_123Q#r=" }
+set_default(:postgresql_password) { "#{application}_123Q" }
 set_default(:postgresql_database) { "#{application}_production" }
 
 namespace :postgresql do
