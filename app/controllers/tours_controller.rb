ca64deec37c377403c403d3793371ae67378f309
bartezic <bartezic@gmail.com>
2013-10-07 14:26:39 +0300

    added random tours and removed long links

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -2,7 +2,8 @@ class ToursController < InheritedResources::Base
   def show
     @tour = Tour.find(params[:id])
     regions_ids = Country.joins(:regions).where('regions.id in (?)', @tour.tour_programs.map(&:region_ids).flatten).map(&:region_ids).flatten
-    @related = Tour.search(params.merge({ region: regions_ids, tour_typ: @tour.tour_types.map(&:id) })).where('tours.id != ?', @tour.id).limit(12)
+    @related = Tour.search(params.merge({ region: regions_ids, tour_typ: @tour.tour_types.map(&:id) })).where('tours.id != ?', @tour.id).limit(6)
+    @random = Tour.find(Tour.where('tours.id != ?', @tour.id).pluck(:id).sample(6))
   end
 
   def index
